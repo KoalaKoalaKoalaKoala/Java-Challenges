58fb4c5215b931e85b5b6550a6c450019691cdda
Dawid <83009949+KoalaKoalaKoalaKoala@users.noreply.github.com>
2022-05-21 21:48:20 +0200

    Add files via upload

---
+++ Abstract Class Challenge/src/MyLinkedList.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+public class MyLinkedList implements NodeList{
+    ListItem root;
+
+    public MyLinkedList(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return this.root;
+    }
+
+    @Override
+    public boolean addItem(ListItem item) {
+
+        if ((String) )
+
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public void traverse(ListItem root) {
+
+    }
+}
